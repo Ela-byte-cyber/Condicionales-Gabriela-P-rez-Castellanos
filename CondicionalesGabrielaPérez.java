ba2cab9ed4a973ed5d891895b141adbb5ff56e17
Ela-byte-cyber <gabrielaperezcastellanos@gmail.com>
2025-04-02 08:19:46 -0500

    Condicionales

---
+++ CondicionalesGabrielaPérez.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class CondicionalesGabrielaPérez {
+    
+    public static void main(string[] args){
+        string danza = "House";
+        
+        if (danza == "hip hop"){
+            System.out.println(x: "Usar ropa overzise");
+            System.out.println(x: "Batallar");
+            System.out.println(x: "Bounce y rocking");
+        
+        }else if (danza == "heels"){
+            System.out.println(x: "Usar ropa ajustada");
+            System.out.println(x: "Usar tacones");
+            System.out.println(x: "Usar el cabello");
+            
+    }else{
+            System.out.println(x: "Es otra danza...");
+        }
+    
+}
